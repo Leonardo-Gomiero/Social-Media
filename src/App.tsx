b97e4e3cf406d29c178e2edaec9559f2a82955ec
Leonardo Gomiero <leoleonardo2000@gmail.com>
2024-06-03 13:37:48 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { Post } from './components/Post';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import myImage from './assets/thats_me.jpeg';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://conteudo.imguol.com.br/c/entretenimento/0c/2023/03/23/john-wick-4-baba-yaga-e-o-mais-novo-capitulo-da-franquia-que-revitalizou-a-carreira-de-keanu-reeves-1679541049583_v2_1x1.png',
+      name: 'Leonardo Capoccia',
+      role: 'Web Dev',
+    },
+    content: [
+      { type: 'paragraph', content: 'Good morrow, everyone' },
+      { type: 'paragraph', content: 'Shall we forge on?' },
+      { type: 'paragraph', content: 'We shant allow ourselves to become complacent. Take each trial as it comes' },
+      { type: 'link', content: 'https://www.youtube.com/watch?v=J9Bm5U-MJZw' },
+    ],
+    publishedAt: new Date('2024-05-24 10:29:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2024/04/22/145359515-b8dd8d8527a1ea3c37b898f1bb94f940.jpg',
+      name: 'John Wick',
+      role: 'Holy Hunter',
+    },
+    content: [
+      { type: 'paragraph', content: 'Happy Birthday, Meleena' },
+      { type: 'paragraph', content: 'Wish you good things' },
+      { type: 'paragraph', content: 'We shant allow ourselves to become complacent. Take each trial as it comes' },
+      { type: 'link', content: 'https://www.youtube.com/watch?v=J9Bm5U-MJZw' },
+    ],
+    publishedAt: new Date('2024-05-18 10:29:00')
+  },
+]
+
+export function App() {
+
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
---
+++ src/components/Comment.tsx
@@ -0,0 +1,51 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0);
+    
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
+    }
+    
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://br.web.img3.acsta.net/pictures/18/07/25/22/12/5616819.jpg" alt=""></Avatar>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Henry Cavill</strong>
+                            <time title="May 11th, 17:55" dateTime="2023-05-20 17:55:30">About 1h ago</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Delete comment">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp onClick={(handleLikeComment)}/>
+                        Applause <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
